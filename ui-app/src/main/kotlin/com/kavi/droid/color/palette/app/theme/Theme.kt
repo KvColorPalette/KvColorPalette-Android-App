15c69a80079c3403c383440001a6bbc10b6135c9
Kavimal Wijewardana <kavimalw@gmail.com>
2025-03-12 15:17:06 -0400

    Add the dynamic theme configuration in jetpack compose theme function

--- ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/theme/Theme.kt
+++ ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/theme/Theme.kt
@@ -1,19 +1,32 @@
 package com.kavi.droid.color.palette.app.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import com.kavi.droid.color.palette.KvColorPalette
 
 @Composable
 fun KvColorPaletteTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
+    val dynamicColor = isDynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
     val appColorScheme = when {
-        darkTheme -> KvColorPalette.colorSchemeThemePalette.darkColorScheme
+        dynamicColor && isDarkTheme -> {
+            dynamicDarkColorScheme(LocalContext.current)
+        }
+        dynamicColor && !isDarkTheme -> {
+            dynamicLightColorScheme(LocalContext.current)
+        }
+        isDarkTheme -> KvColorPalette.colorSchemeThemePalette.darkColorScheme
         else -> KvColorPalette.colorSchemeThemePalette.lightColorScheme
     }
 
