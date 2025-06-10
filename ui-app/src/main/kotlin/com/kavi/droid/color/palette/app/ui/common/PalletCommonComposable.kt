80e6e4a4f5a9389824b00f6684050f6a9a0fc7dd
Kavimal Wijewardana <kavimalw@gmail.com>
2025-06-09 22:34:22 -0400

    Remove unwanted import

--- ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/ui/common/PalletCommonComposable.kt
+++ ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/ui/common/PalletCommonComposable.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalClipboard
-import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
